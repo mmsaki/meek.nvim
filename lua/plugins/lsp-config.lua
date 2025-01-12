eb2ce4e8a6440820387d7e1f6b6ddc68ac12dbef
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-01-12 07:33:41 -0600

    📦 updated lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,9 +2,27 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	dependencies = {
 		"williamboman/mason.nvim",
+		"hrsh7th/cmp-nvim-lsp",
 		"neovim/nvim-lspconfig",
+		{ "folke/neoconf.nvim" },
+		{
+			"creativenull/efmls-configs-nvim",
+			version = "v1.x.x", -- version is optional, but recommended
+		},
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 	},
 	config = function()
+		require("neoconf").setup({})
 		local mason = require("mason")
 		mason.setup({
 			ui = {
@@ -16,7 +34,7 @@ return {
 			},
 		})
 
-		require("mason-lspconfig").setup {
+		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
 				"rust_analyzer",
@@ -28,16 +46,15 @@ return {
 				"tailwindcss",
 				"solidity_ls",
 			},
-		}
+		})
 
-		local lspconfig = require("lspconfig")
 		require("lspconfig").lua_ls.setup({})
-		vim.api.nvim_create_autocmd('FileType', {
+		vim.api.nvim_create_autocmd("FileType", {
 			-- This handler will fire when the buffer's 'filetype' is "python"
-			pattern = 'solidity',
+			pattern = "solidity",
 			callback = function(args)
 				vim.lsp.start({
-					name = '@msaki solidity lsp server',
+					name = "@msaki solidity lsp server",
 					-- cmd = { "vscode-solidity-server", "--stdio" },
 					cmd = { "nc", "localhost", "65432" }, -- NOTE: should be started manually
 
@@ -45,85 +62,110 @@ return {
 					-- current buffer (`args.buf`) that contains either a "setup.py" or a
 					-- "pyproject.toml" file. Files that share a root directory will reuse
 					-- the connection to the same LSP server.
-					root_dir = vim.fs.root(args.buf, { 'foundry.toml', '.git' }),
-		 			settings = {
-		 				wake = {
-		 					configuration = {
-		 						use_toml_if_present = true,
-		 						toml_path = "wake.toml",
-		 					},
-		 					lsp = {
-		 						compilation_delay = 0,
-		 						find_references = {
-		 							include_declarations = true,
-		 						},
-		 						code_lens = {
-		 							enable = false,
-		 						},
-		 						detectors = {
-		 							only = {},
-		 						},
-		 					},
-		 				},
-		 			}
+					root_dir = vim.fs.root(args.buf, { "foundry.toml", ".git" }),
+					settings = {
+						wake = {
+							configuration = {
+								use_toml_if_present = true,
+								toml_path = "wake.toml",
+							},
+							lsp = {
+								compilation_delay = 0,
+								find_references = {
+									include_declarations = true,
+								},
+								code_lens = {
+									enable = false,
+								},
+								detectors = {
+									only = {},
+								},
+							},
+						},
+					},
 				})
-		 	end,
+			end,
 		})
+		-- Register linters and formatters per language
+		local languages = {
+			typescript = {
+				require("efmls-configs.linters.eslint"),
+				require("efmls-configs.formatters.prettier"),
+			},
+			solidity = {
+				require("efmls-configs.linters.solhint"),
+				require("efmls-configs.formatters.prettier"),
+			},
+			lua = {
+				require("efmls-configs.formatters.stylua"),
+			},
+		}
+
+		-- Or use the defaults provided by this plugin
+		-- check doc/SUPPORTED_LIST.md for all the defaults provided
+		--
+		-- local languages = require('efmls-configs.defaults').languages()
+
+		local efmls_config = {
+			filetypes = vim.tbl_keys(languages),
+			settings = {
+				rootMarkers = { ".git/" },
+				languages = languages,
+			},
+			init_options = {
+				documentFormatting = true,
+				documentRangeFormatting = true,
+				hover = true,
+				completion = true,
+				codeAction = true,
+				documentSymbol = true,
+			},
+		}
+
+		require("lspconfig").efm.setup(vim.tbl_extend("force", efmls_config, {
+			-- Pass your cutom config below like on_attach and capabilities
+			--
+			-- on_attach = on_attach,
+			-- capabilities = capabilities,
+		}))
 		-- on lsp attach
-		-- vim.api.nvim_create_autocmd('LspAttach', {
-		-- 	callback = function(args)
-		-- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		-- 		if client.supports_method('textDocument/implementation') then
-		-- 			-- Create a keymap for vim.lsp.buf.implementation
-		-- 		end
+		local vim_fmt_autogroup = vim.api.nvim_create_augroup("LspFormattingGroup", { clear = false })
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim_fmt_autogroup,
+			callback = function(args)
+				vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if client.supports_method("textDocument/implementation") then
+					-- Create a keymap for vim.lsp.buf.implementation
+					local function on_list(options)
+						vim.fn.setqflist({}, " ", options)
+						vim.cmd.cfirst()
+					end
 
-		-- 		if client.supports_method('textDocument/completion') then
-		-- 			-- Enable auto-completion
-		-- 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-		-- 		end
+					vim.lsp.buf.implementation({ on_list = on_list })
+				end
 
-		-- 		if client.supports_method('textDocument/formatting') then
-		-- 			-- Format the current buffer on save
-		-- 			vim.api.nvim_create_autocmd('BufWritePre', {
-		-- 				buffer = args.buf,
-		-- 				callback = function()
-		-- 					vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-		-- 				end,
-		-- 			})
-		-- 		end
-		-- 	end,
-		-- })
-		--		require("lspconfig").solidity_ls.setup {
-		--			cmd = { "vscode-solidity-server", "--stdio" },
-		--			filetypes = { "solidity" },
-		--			single_file_support = true,
-		--		}
-		--	require("lspconfig.configs").wake = {
-		--		default_config = {
-		--			cmd = { "wake", "lsp" }, -- NOTE: should be started manually
-		--			filetypes = { "solidity" },
-		--			root_dir = lspconfig.util.root_pattern("foundry.toml"),
-		--			settings = {
-		--				wake = {
-		--					configuration = {
-		--						use_toml_if_present = true,
-		--						toml_path = "wake.toml",
-		--					},
-		--					lsp = {
-		--						compilation_delay = 15,
-		--						find_references = {
-		--							include_declarations = true,
-		--						},
-		--						code_lens = {
-		--							enable = false,
-		--						},
-		--						detectors = {
-		--							only = {},
-		--						},
-		--					},
-		--				},
-		--			},
-		--		},
-		--	}
+				if client.supports_method("textDocument/completion") then
+					-- Enable auto-completion
+					vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+				end
+
+				if client.supports_method("textDocument/formatting") then
+					-- Format the current buffer on save
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = args.buf,
+						callback = function()
+							local efm = vim.lsp.get_clients({ name = "efm" })
+
+							if vim.tbl_isempty(efm) then
+								return
+							end
+
+							vim.lsp.buf.format({ name = "efm", async = true, bufnr = args.buf, id = client.id })
+						end,
+					})
+				end
+			end,
+		})
 	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,19 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
 	lazy = false,
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		-- local telescope
 		local builtin = require("telescope.builtin")
+		vim.keymap.set('n', '<leader>ff', builtin.find_files)
+		vim.keymap.set('n', '<leader>en', function()
+			builtin.find_files {
+				cwd = vim.fn.stdpath('config')
+			}
+		end)
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+		vim.keymap.set('n', '<leader>fb', builtin.buffers)
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 	end,
-	keys = {
-		vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", {}),
-		vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<CR>", {}),
-		vim.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>", {}),
-		vim.keymap.set('n', '<leader>fh', ":Telescope help_tags<CR>", {}),
-	}
 }
