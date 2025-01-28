db174c704c14d230f082f8f6dc9ce5b7a1deb430
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-01-27 19:42:12 -0600

    🍜 chore: plugin updates

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,9 +1,23 @@
-return { 
-  "catppuccin/nvim",
-  name = "catppuccin",
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
 	lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			-- transparent_background = true,
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
