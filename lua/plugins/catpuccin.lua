5de49768ee2d72dc6760c8353596bc6d5dddd522
Meek Msaki <meek10x@gmail.com>
2025-05-23 14:05:43 -0500

    refactoring, clean up lsp and added completion and autformat plugin

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,23 +1,40 @@
-return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			-- transparent_background = true,
-			background = { -- :h background
-				light = "latte",
-				dark = "mocha",
-			},
-			color_overrides = {
-				mocha = {
-					base = "#000000",
-					mantle = "#000000",
-					crust = "#000000",
-				},
-			},
-		})
-		vim.cmd.colorscheme("catppuccin")
-	end,
+-- return {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("catppuccin").setup({
+-- 			-- transparent_background = true,
+-- 			background = { -- :h background
+-- 				light = "latte",
+-- 				dark = "mocha",
+-- 			},
+-- 			color_overrides = {
+-- 				mocha = {
+-- 					base = "#000000",
+-- 					mantle = "#000000",
+-- 					crust = "#000000",
+-- 				},
+-- 			},
+-- 		})
+-- 		vim.cmd.colorscheme("catppuccin")
+-- 	end,
+-- }
+return { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'folke/tokyonight.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,15 +8,13 @@ return {
     "saghen/blink.cmp",
     {
       "creativenull/efmls-configs-nvim",
-      version = "v1.x.x", -- version is optional, but recommended
+      version = "v1.x.x",
     },
     {
       "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
+      ft = "lua",
       opts = {
         library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
@@ -72,60 +70,22 @@ return {
         },
       },
     })
-
-    -- vim.api.nvim_create_autocmd("FileType", {
-    -- 	-- This handler will fire when the buffer's 'filetype' is "python"
-    -- 	pattern = "solidity",
-    -- 	callback = function(args)
-    -- 		vim.lsp.start({
-    -- 			name = "Wake solidity lsp server",
-    -- 			cmd = { "nc", "localhost", "65432" }, -- NOTE: should be started manually
-    -- 			-- cmd = { "solc", "--lsp", "--base-path", ".", "--include-paths", "./lib/" },
-    -- 			-- Set the "root directory" to the parent directory of the file in the
-    -- 			-- current buffer (`args.buf`) that contains either a "setup.py" or a
-    -- 			-- "pyproject.toml" file. Files that share a root directory will reuse
-    -- 			-- the connection to the same LSP server.
-    -- 			root_dir = vim.fs.root(args.buf, { "foundry.toml", ".git" }),
-    -- 			settings = {
-    -- 				wake = {
-    -- 					configuration = {
-    -- 						use_toml_if_present = true,
-    -- 						toml_path = "wake.toml",
-    -- 					},
-    -- 					lsp = {
-    -- 						compilation_delay = 0,
-    -- 						find_references = {
-    -- 							include_declarations = true,
-    -- 						},
-    -- 						code_lens = {
-    -- 							enable = false,
-    -- 						},
-    -- 						detectors = {
-    -- 							enable = true,
-    -- 						},
-    -- 					},
-    -- 				},
-    -- 			},
-    -- 		})
-    -- 	end,
-    -- })
-    -- Register linters and formatters per language
     local languages = {
       javascript = {
         require("efmls-configs.linters.eslint"),
-        require("efmls-configs.formatters.biome"),
+        require("efmls-configs.formatters.prettier_d"),
       },
       javascriptreact = {
         require("efmls-configs.linters.eslint"),
-        require("efmls-configs.formatters.biome"),
+        require("efmls-configs.formatters.prettier_d"),
       },
       typescript = {
         require("efmls-configs.linters.eslint"),
-        require("efmls-configs.formatters.biome"),
+        require("efmls-configs.formatters.prettier_d"),
       },
       typescriptreact = {
         require("efmls-configs.linters.eslint"),
-        require("efmls-configs.formatters.biome"),
+        require("efmls-configs.formatters.prettier_d"),
       },
       solidity = {
         require("efmls-configs.linters.solhint"),
@@ -153,12 +113,6 @@ return {
         require("efmls-configs.formatters.mdformat"),
       },
     }
-
-    -- Or use the defaults provided by this plugin
-    -- check doc/SUPPORTED_LIST.md for all the defaults provided
-    --
-    -- local languages = require('efmls-configs.defaults').languages()
-
     local efmls_config = {
       filetypes = vim.tbl_keys(languages),
       settings = {
@@ -174,52 +128,8 @@ return {
         documentSymbol = true,
       },
     }
-
     lspconfig.efm.setup(vim.tbl_extend("force", efmls_config, {
-      -- Pass your cutom config below like on_attach and capabilities
-      --
-      -- on_attach = on_attarh,
       capabilities = capabilities,
     }))
-    -- on lsp attach
-    local vim_fmt_autogroup = vim.api.nvim_create_augroup("LspFormattingGroup", { clear = false })
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim_fmt_autogroup,
-      callback = function(args)
-        vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-        if client.supports_method("textDocument/implementation", args.buf) then
-          -- Create a keymap for vim.lsp.buf.implementation
-          local function on_list(options)
-            vim.fn.setqflist({}, " ", options)
-            vim.cmd.cfirst()
-          end
-
-          vim.lsp.buf.implementation({ on_list = on_list })
-        end
-
-        if client.supports_method("textDocument/completion", args.buf) then
-          -- Enable auto-completion
-          vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        end
-
-        if client.supports_method("textDocument/formatting", args.buf) then
-          -- Format the current buffer on save
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = args.buf,
-            callback = function()
-              local efm = vim.lsp.get_clients({ name = "efm" })
-
-              if vim.tbl_isempty(efm) then
-                return
-              end
-
-              vim.lsp.buf.format({ name = "efm", async = true, bufnr = args.buf, id = client.id })
-            end,
-          })
-        end
-      end,
-    })
   end,
 }
